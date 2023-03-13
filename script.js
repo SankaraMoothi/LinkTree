3303737406a1b96f9d16939a292d725c7d5699ed
SankaraMoothi <sankaramoorthi99@gmail.com>
2023-03-13 21:44:06 +0530

    firstCommit

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById("git").onclick = function () {
+  window.open("https://github.com/SankaraMoothi", "_blank");
+};
+document.getElementById("Portfolio").onclick = function () {
+  window.open("https://sankar-portfolio-react.netlify.app/", "_blank");
+};
+document.getElementById("email").onclick = function () {
+  window.open(
+    "https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSGMvfvkNCJpcrcDRJXhwKskzNVzVVCjstVgcSzGTMgWwmJbnPgPLbSsBLFdcbLhHfRFLmhW",
+    "_blank"
+  );
+};
+document.getElementById("insta").onclick = function () {
+  window.open(
+    "https://www.instagram.com/mass__naruto__/?igshid=YmMyMTA2M2Y%3D",
+    "_blank"
+  );
+};
